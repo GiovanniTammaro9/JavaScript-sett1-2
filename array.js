77703a03ed62658c052653bb19c845df5c0df43e
GiovanniTammaro9 <gio_play@live.it>
2024-11-08 13:45:12 +0100

    sommati i numeri dell'array

--- array.js
+++ array.js
@@ -1,2 +1,9 @@
 const myArray = [1, 2, 3, 4, 5];
 console.log(myArray);
+//somma dei valori nell'array
+let somma = 0;
+
+for (let i = 0; i < myArray.length; i++){
+    somma += myArray[i];
+}
+console.log("la somma degli elementi dell'array è" + somma);
