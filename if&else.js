628a7cbc6de8167656cef2b1d70d2ca36a1d1b3f
GiovanniTammaro9 <gio_play@live.it>
2024-11-08 13:27:45 +0100

    utilizzato lo switch

--- if&else.js
+++ if&else.js
@@ -1,22 +1,38 @@
 let number = 10;
 
-if(number > 5){
+if (number > 5) {
     console.log("la variabile è maggiore di 5");
-}else {
+} else {
     console.log("la variabile è minore di 5");
 }
 
 //if ed else if
 
 let score = parseInt(prompt("inserisci il numero"));
-if(score >= 90 && score <= 100){
+if (score >= 90 && score <= 100) {
     console.log("voto ottimo ");
-} else if (score >= 70 && score <=89){
+} else if (score >= 70 && score <= 89) {
     console.log("voto buono");
-}else if (score >= 60 && score >= 69){
-    console.log ("voto sufficente");
-}else if (score <= 59){
-    console.log ("voto insufficente");
-}else {
+} else if (score >= 60 && score >= 69) {
+    console.log("voto sufficente");
+} else if (score <= 59) {
+    console.log("voto insufficente");
+} else {
     console.log("punteggio non valido");
+}
+
+//switch case 
+switch (true) {
+    case (score >= 90 && score <= 100):
+        console.log("il voto è ottimo");
+        break;
+    case (score >= 70 && score <= 89):
+        console.log("voto buono");
+        break;
+    case (score >= 60 && score <= 69):
+        console.log("voto sufficente");
+        break;
+    case (score >= 59):
+        console.log("voto insufficente");
+        break;
 }
